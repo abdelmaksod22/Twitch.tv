dfe95ae903b9627ab64d54c0e2caa15226fff81f
Mo Abdelmaksod <m.abdelmaksod97@yahoo.com>
2024-10-02 03:08:10 +0200

    adding rtmp-server and stream logic for my app

--- server/src/controllers/channels/getChannelDetails.js
+++ server/src/controllers/channels/getChannelDetails.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 import User from "../../models/User.js";
 import Channel from "../../models/Channel.js";
 
@@ -9,8 +11,25 @@ export const getChannelDetails = async (req, res) => {
       return res.status(404).send("Channel Not found");
     }
     const user = await User.findOne({ channel: channelId }, { username: 1 });
-    const streamUrl = "http";
-    const isOnline = false;
+
+    const streamUrl = `http://localhost:8000/live/${channel.streamKey}.flv`;
+
+    const requestData = await axios.get("http://localhost:8000/api/streams");
+
+    const activeStreams = requestData.data;
+
+    let liveStreams = [];
+
+    for (const streamId in activeStreams?.live) {
+      if (
+        activeStreams.live[streamId].publisher &&
+        activeStreams.live[streamId].publisher !== null
+      ) {
+        liveStreams.push(streamId);
+      }
+    }
+
+    const isOnline = liveStreams.includes(channel.streamKey);
     return res.status(201).json({
       id: channel._id,
       title: channel.title,
--- server/src/controllers/channels/getChannels.js
+++ server/src/controllers/channels/getChannels.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 import User from "../../models/User.js";
 
 export const getChannels = async (req, res) => {
@@ -6,15 +8,29 @@ export const getChannels = async (req, res) => {
       "channel"
     );
 
+    const requestData = await axios.get("http://localhost:8000/api/streams");
+    const activeStreams = requestData.data;
+
+    let liveStreams = [];
+
+    for (const streamId in activeStreams?.live) {
+      if (
+        activeStreams.live[streamId].publisher &&
+        activeStreams.live[streamId].publisher !== null
+      ) {
+        liveStreams.push(streamId);
+      }
+    }
+
     const channels = users
-      //   .filter((u) => u.channel.isActive)
+      .filter((u) => u.channel.isActive)
       .map((user) => {
         return {
           id: user.channel._id,
           title: user.channel.title,
           avatarUrl: user.channel.avatarUrl,
           username: user.username,
-          isOnline: false,
+          isOnline: liveStreams.includes(user.channel.streamKey),
         };
       });
     return res.json({ channels });
